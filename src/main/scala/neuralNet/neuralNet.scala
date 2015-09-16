6822e50a8861ce853d5cf29310fe75f3934c3381
Xander Dunn <xander@osaro.com>
2015-09-15 20:40:27 -0700

    Fix a typo

--- src/main/scala/neuralNet/neuralNet.scala
+++ src/main/scala/neuralNet/neuralNet.scala
@@ -8,7 +8,7 @@ package neuralNet {
 /** A multi-layer perceptron defined by the layout [number of input neuron, number of hidden neurons, number of outpout neurons].  The layout must have at least an input, output and single hidden layer.  All extra numbers are used as additional hidden layers. */
 class MultiLayerPerceptron(layout : List[Int], activationFunctions : List[ActivationFunction]) {
   require(layout.size >= 3, "There must be input layer, output layer, and hidden layer sizes specified at the very least.")
-  require(layout.size == activationFunctions.size - 1, "You must define an activation function for all put the input layer.")
+  require(layout.size == activationFunctions.size - 1, "You must define an activation function for all but the input layer.")
 
   def feedForward(inputs : List[Double]) : List[Double] = {
     List()
